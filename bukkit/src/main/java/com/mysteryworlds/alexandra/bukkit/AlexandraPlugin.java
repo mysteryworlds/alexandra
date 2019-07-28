59c3c057e409cd853715eb660ce23c1bfd0eef13
Felix Klauke <info@felix-klauke.de>
2019-07-28 18:33:44 +0200

    Setup metrics on startup.

--- bukkit/src/main/java/com/mysteryworlds/alexandra/bukkit/AlexandraPlugin.java
+++ bukkit/src/main/java/com/mysteryworlds/alexandra/bukkit/AlexandraPlugin.java
@@ -5,6 +5,7 @@ import com.mysteryworlds.alexandra.bukkit.service.ChatServiceImpl;
 import com.mysteryworlds.alexandra.bukkit.vault.VaultChat;
 import net.milkbowl.vault.chat.Chat;
 import net.milkbowl.vault.permission.Permission;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.ServicesManager;
@@ -21,7 +22,8 @@ public class AlexandraPlugin extends JavaPlugin {
         ChatService chatService = new ChatServiceImpl(null);
 
         // Get Permission service
-        RegisteredServiceProvider<Permission> registration = servicesManager.getRegistration(Permission.class);
+        RegisteredServiceProvider<Permission> registration = servicesManager
+            .getRegistration(Permission.class);
         if (registration == null) {
             getLogger().severe("Couldn't find permissions. Disabling plugin.");
             getServer().getPluginManager().disablePlugin(this);
@@ -34,10 +36,18 @@ public class AlexandraPlugin extends JavaPlugin {
         getLogger().info("Hooking into vault.");
         Chat chat = new VaultChat(this, permission, chatService);
         servicesManager.register(Chat.class, chat, this, ServicePriority.Highest);
+
+        // Setup metrics
+        setupMetrics();
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    private void setupMetrics() {
+
+        new Metrics(this);
     }
 }
